c01c4a6b83047a7aaad7036828b07d35c4cd8fb5
KhalidaG2024 <khalida.belgroune.epouse.ghanem@umontreal.ca>
2024-11-05 23:44:06 -0500

    Update student_code.py

--- student_code.py
+++ student_code.py
@@ -1,5 +1,10 @@
 def decrypt(C):
-  M=""
-  #entrez votre code ici.
-  #Vous pouvez créer des fonctions auxiliaires et adapter le code à votre façon mais decrypt dois renvoyer le message décrypté
-  return M
+  M= []
+    for i in range(0, len(C), 8):  # Traiter 8 bits d'un coup
+        binary_sequence = C[i:i+8]
+        decryption_key={v: k for k, v in encryption_key.items()}
+        symbol = decryption_key.get(binary_sequence, None)
+        if symbol:
+            M.append(symbol)
+    return ''.join(M)
+  
